3c8ce043f5f37aa6ac582281a8f02ac0b9491a62
theCieniu <pweiher@wp.eu>
2019-08-29 21:09:06 +0200

    responsive navigation
    responsive and interactive navigation - finished!

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var i = 0;
 text = "Welcome to the Rossa Florist";
 
 function typing() {
-  if (i < text.length) {
+    if (i < text.length) {
     document.getElementById("typing").innerHTML += text.charAt(i);
     i++;
     setTimeout(typing, 100);
@@ -14,4 +14,29 @@ function typing() {
 typing()
 
 
-// 
+//
+
+
+let burger = document.getElementById("burger");
+let menu = document.getElementById("menu");
+let overlay = document.getElementById("overlay");
+
+burger.addEventListener("click", opened);
+
+overlay.addEventListener("click", closed);
+
+menu.addEventListener("click", closed);
+
+function opened() {
+  overlay.classList.remove("opennav");
+  menu.classList.remove("opennav");
+  burger.classList.add("opennav");
+  console.log("open")
+}
+
+function closed() {
+  overlay.classList.add("opennav");
+  menu.classList.add("opennav");
+  burger.classList.remove("opennav");
+  console.log("closednav")
+}
